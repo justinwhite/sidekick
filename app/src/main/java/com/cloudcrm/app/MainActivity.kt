4fd7036df090314f914b613ebcd3a1056909b7a6
Justin White <justin@pensarbahamas.com>
2026-08-31 13:03:46 -0400

    Fix: Enable edge-to-edge window for proper IME inset padding

--- app/src/main/java/com/cloudcrm/app/MainActivity.kt
+++ app/src/main/java/com/cloudcrm/app/MainActivity.kt
@@ -3,7 +3,9 @@ package com.cloudcrm.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
@@ -45,6 +47,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             CloudCrmTheme {
                 MainAppContainer(viewModel = viewModel)
@@ -108,7 +111,7 @@ fun MainAppContainer(viewModel: CloudCrmViewModel) {
         NavHost(
             navController = navController,
             startDestination = Screen.Capture.route,
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier.padding(innerPadding).consumeWindowInsets(innerPadding)
         ) {
             composable(Screen.Capture.route) {
                 CaptureScreen(
